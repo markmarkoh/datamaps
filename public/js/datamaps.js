bcecdb11114a067d1620a74d1b2d131994827e71
Mark DiMarco <mark.dimarco@gmail.com>
2013-06-13 16:37:49 -0500

    updated home.html demo

--- public/js/datamaps.js
+++ public/js/datamaps.js
@@ -1,13 +1,12 @@
+var datamaps = {};
 (function() {
   // Leak datamaps object globally
-  var datamaps = this.datamaps = {};
-
-  var svg, projection, path;
+  var svg;
 
 
 
   function addContainer( element ) {
-    svg = d3.select( element ).append('svg')
+    this.svg = d3.select( element ).append('svg')
       .attr('width', element.offsetWidth)
       .attr('height', element.offsetHeight);
   }
@@ -17,68 +16,80 @@
       .html('path {stroke: #FFFFFF; stroke-width: 1px;} .hoverover {display: none; font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; } .hoverinfo {padding: 4px; border-radius: 1px; background-color: #FFF; box-shadow: 1px 1px 5px #CCC; font-size: 12px; border: 1px solid #CCC; } .hoverinfo hr {border:1px dotted #CCC; }');
   }
 
-  function drawSubunits( data, options) {
-  var fillData = options.fills,
-      colorCodeData = options.data || {},
-      geoConfig = options.geographyConfig;
+  function drawSubunits( data ) {
+    var fillData = this.options.fills,
+        colorCodeData = this.options.data || {},
+        geoConfig = this.options.geographyConfig;
 
-   svg.append('g')
-        .attr('class', 'subunits')
-        .selectAll('.subunit')
-          .data(topojson.feature( data, data.objects['states-topo'] ).features )
-          .enter().append('path')
-            .attr('d', path)
-            .attr('class', function(d) {
-              return 'subunit ' + d.id;
-            })
-            .attr('data-info', function(d) {
-              return JSON.stringify( colorCodeData[d.id]);
-            })
-            .style('fill', function(d) {
-              var fillColor;
 
-              if ( colorCodeData[d.id] ) {
-                fillColor = fillData[ colorCodeData[d.id].fillKey ];
-              }
+    var subunits = d3.select('g.subunits');
+    if ( subunits.empty() ) {
+      subunits = this.svg.append('g').attr('class', 'subunits');
+    }
 
-              return fillColor || fillData.defaultFill; 
-            })
-            .style('stroke-width', geoConfig.borderWidth)
-            .style('stroke', geoConfig.borderColor);
+    var geoData = topojson.feature( data, data.objects[ this.options.scope ] ).features;
+    console.log(geoConfig);
+    if ( geoConfig.hideAntarctica ) {
+      console.log('here');
+      geoData = geoData.filter(function(feature) {
+        console.log(feature);
+        return feature.id !== "ATA";
+      });
+    }
+
+    var geo = subunits.selectAll('path.subunit').data( geoData );
+
+    geo.enter()
+      .append('path')
+      .attr('d', this.path)
+      .attr('class', function(d) {
+        return 'subunit ' + d.id;
+      })
+      .attr('data-info', function(d) {
+        return JSON.stringify( colorCodeData[d.id]);
+      })
+      .style('fill', function(d) {
+        var fillColor;
+
+        if ( colorCodeData[d.id] ) {
+          fillColor = fillData[ colorCodeData[d.id].fillKey ];
+        }
+
+        return fillColor || fillData.defaultFill;
+      })
+      .style('stroke-width', geoConfig.borderWidth)
+      .style('stroke', geoConfig.borderColor);
   }
 
-  function handleGeographyConfig ( geoConfig ) {
+  function handleGeographyConfig ( options ) {
     var hoverover;
 
-    if ( geoConfig.popupOnHover ) {
+    if ( options.popupOnHover ) {
       hoverover = d3.select( 'body' ).append('div')
         .attr('class', 'hoverover')
         .style('z-index', 10001)
-        .style('position', 'absolute').text('hi');
+        .style('position', 'absolute');
     }
 
-    if ( geoConfig.highlightOnHover || geoConfig.popupOnHover ) {
-      svg.selectAll('.subunit')
+    if ( options.highlightOnHover || options.popupOnHover ) {
+      this.svg.selectAll('.subunit')
         .on('mouseover', function(d) {
           var $this = d3.select(this);
 
-          if ( geoConfig.highlightOnHover ) {
+          if ( options.highlightOnHover ) {
+            var previousAttributes = {
+              'fill':  $this.style('fill'),
+              'stroke': $this.style('stroke'),
+              'stroke-width': $this.style('stroke-width'),
+              'fill-opacity': $this.style('fill-opacity')
+            };
+
             $this
-              .attr('previous-fill', function() {
-                var previousFill = $this.style('fill');
-                $this.style('fill', geoConfig.highlightFillColor);
-                return previousFill;
-              })
-              .attr('previous-stroke', function() {
-                var previousStroke = $this.style('stroke');
-                $this.style('stroke', geoConfig.highlightBorderColor);
-                return previousStroke;
-              })
-              .attr('previous-stroke-width', function() {
-                var previousStrokeWidth = $this.style('stroke-width');
-                $this.style('stroke-width', geoConfig.highlightBorderWidth);
-                return previousStrokeWidth;
-              });
+              .style('fill', options.highlightFillColor)
+              .style('stroke', options.highlightBorderColor)
+              .style('stroke-width', options.highlightBorderWidth)
+              .style('fill-opacity', options.highlightFillOpacity)
+              .attr('data-previousAttributes', JSON.stringify(previousAttributes));
 
             /* remove the element and place it at the bottom
                 of the parent since the borders will likely be clipped */
@@ -88,15 +99,14 @@
             parentEl.appendChild(el);
           }
 
-          if ( geoConfig.popupOnHover ) {
+          if ( options.popupOnHover ) {
 
               $this.on('mousemove', function() {
                 var position = d3.mouse(this);
                 d3.select('.hoverover')
-                  .style('top', (position[1] + 30) + "px")            
+                  .style('top', (position[1] + 30) + "px")
                   .html(function() {
                     var data = JSON.parse($this.attr('data-info'));
-
                     if (data === null) {
                       return '';
                     }
@@ -105,7 +115,7 @@
                     }
 
                   })
-                  .style('left', position[0] + "px");           
+                  .style('left', position[0] + "px");
               });
 
               d3.select('.hoverover').style('display', 'block');
@@ -113,46 +123,148 @@
         })
         .on('mouseout', function() {
           var $this = d3.select(this);
-          $this
-            .style('fill', function() {
-              return $this.attr('previous-fill');
-            })
-            .style('stroke', function() {
-              return $this.attr('previous-stroke');
-            })
-            .style('stroke-width', function() {
-              return $this.attr('previous-strokewidth');
-            });
 
+          if (options.highlightOnHover) {
+            //reapply previous attributes
+            var previousAttributes = JSON.parse( $this.attr('data-previousAttributes') );
+            for ( var attr in previousAttributes ) {
+              $this.style(attr, previousAttributes[attr]);
+            }
+          }
           d3.select('.hoverover').style('display', 'none');
         });
     }
 
   }
 
+  function handleBubbles ( bubbleData ) {
+    var self = this,
+        fillData = this.options.fills,
+        options = this.options.bubbleConfig;
+
+    if ( !bubbleData || (bubbleData && !bubbleData.slice) ) {
+      throw "Datamaps Error - bubbles must be an array";
+    }
+
+    var bubblesContainer = d3.select('g.bubbles-container');
+    if ( bubblesContainer.empty() ) {
+      bubblesContainer = this.svg.append('g').attr('class', 'bubbles-container');
+    }
+
+    var bubbles = bubblesContainer.selectAll('circle.bubble').data( bubbleData, JSON.stringify );
+
+    bubbles
+      .enter()
+        .append('svg:circle')
+        .attr('class', 'bubble')
+        .attr('cx', function ( datum ) {
+          console.log('calc', datum);
+          return self.projection([datum.longitude, datum.latitude])[0];
+        })
+        .attr('cy', function ( datum ) {
+          return self.projection([datum.longitude, datum.latitude])[1];
+        })
+        .attr('r', 0) //for animation purposes
+        .style('stroke', options.borderColor)
+        .style('stroke-width', options.borderWidth)
+        .style('opacity', options.fillOpacity)
+        .style('fill', function ( datum ) {
+          var fillColor = fillData[ datum.fillKey ];
+          return fillColor || fillData.defaultFill;
+        })
+        .on('mouseover', function ( datum ) {
+          var $this = d3.select(this);
+
+          if (options.highlightOnHover) {
+            //save all previous attributes for mouseout
+            var previousAttributes = {
+              'fill':  $this.style('fill'),
+              'stroke': $this.style('stroke'),
+              'stroke-width': $this.style('stroke-width'),
+              'fill-opacity': $this.style('fill-opacity')
+            };
+
+            $this
+              .style('fill', options.highlightFillColor)
+              .style('stroke', options.highlightBorderColor)
+              .style('stroke-width', options.highlightBorderWidth)
+              .style('fill-opacity', options.highlightFillOpacity)
+              .attr('data-previousAttributes', JSON.stringify(previousAttributes));
+          }
+        })
+        .on('mouseout', function ( datum ) {
+          var $this = d3.select(this);
+
+          if (options.highlightOnHover) {
+            //reapply previous attributes
+            var previousAttributes = JSON.parse( $this.attr('data-previousAttributes') );
+            for ( var attr in previousAttributes ) {
+              $this.style(attr, previousAttributes[attr]);
+            }
+          }
+        })
+        .transition().duration(400)
+          .attr('r', function ( datum ) {
+            return datum.radius;
+          });
+
+    bubbles.exit()
+      .transition()
+        .attr("r", 0)
+        .remove()
+
+  }
+
   function setProjection( scope, element ) {
     if ( scope === 'usa' ) {
-      projection = d3.geo.albersUsa()
+      this.projection = d3.geo.albersUsa()
         .scale(element.offsetWidth)
         .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
     }
+    else if ( scope === 'world' ) {
+      this.projection = d3.geo.equirectangular()
+        .scale(element.offsetWidth / 6)
+        .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    }
 
-    path = d3.geo.path()
-      .projection( projection );
+    this.path = d3.geo.path()
+      .projection( this.projection );
   }
 
-  datamaps.draw = function( options ) {
+  /**************************************
+
+    Global Functions
+
+  ***************************************/
+
+  this.setBubbles = function ( bubbles ) {
+    handleBubbles.call(this, bubbles);
+  };
+
+  this.draw = function( options ) {
+    //save off in a closure
+    var self = this;
+
+    //set options for global use
+    this.options = options;
+
+    //add the SVG container
     if ( d3.select( options.element ).select('svg').length > 0 ) {
-      addContainer( options.element );
+      addContainer.call(this, options.element );
     }
+
+    //add the style blocks for the hoverover
     addStyle();
-    setProjection( options.scope, options.element );
 
-    d3.json('/public/js/app/data/testing.json', function(error, result) {
-        drawSubunits( result, options );   
+    //set projections and paths based on scope
+    setProjection.apply(this, [options.scope, options.element] );
 
-        handleGeographyConfig ( options.geographyConfig );
-
+    d3.json('/public/js/app/data/' + options.scope + '.topo.json', function(error, result) {
+        drawSubunits.apply(self, [result, options] );
+        handleGeographyConfig.call(self, options.geographyConfig );
+        self.setBubbles ( options.bubbles );
     });
   };
-})();
+
+
+}).apply(datamaps);
