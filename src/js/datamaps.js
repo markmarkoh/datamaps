f6c012ecfdf80b2879ece05e0074b0e7bda5be73
lulzia <nunziab88@gmail.com>
2016-07-22 09:34:29 +0200

    Change if in switch

--- src/js/datamaps.js
+++ src/js/datamaps.js
@@ -375,25 +375,61 @@
             var originXY, destXY;
 
             if (typeof datum.origin === "string") {
-              if (datum.origin == "USA") { originXY = self.latLngToXY(41.140276, -100.760145); }
-              else if (datum.origin == "CAN") { originXY = self.latLngToXY(56.624472, -114.665293); }
-              else if (datum.origin == "JPN") { originXY = self.latLngToXY(35.689487, 139.691706); }
-              else if (datum.origin == "CHL") { originXY = self.latLngToXY(-33.448890, -70.669265); }
-              else if (datum.origin == "IDN") { originXY = self.latLngToXY(-6.208763, 106.845599); }
-              else if (datum.origin == "MYS") { originXY = self.latLngToXY(14.599512, 120.984219); }
-              else { originXY = self.path.centroid(svg.select('path.' + datum.origin).data()[0]); }
+               switch (datum.origin) {
+                    case  "USA":
+                        originXY = self.latLngToXY(41.140276, -100.760145)
+                        break;
+                    case "CAN":
+                        originXY = self.latLngToXY(56.624472, -114.665293);
+                        break;
+                    case "JPN":
+                        originXY = self.latLngToXY(35.689487, 139.691706);
+                        break;
+                    case "CHL":
+                        originXY = self.latLngToXY(-33.448890, -70.669265);
+                        break;
+                    case "IDN":
+                        originXY = self.latLngToXY(-6.208763, 106.845599);
+                        break;
+                    case "MYS":
+                        originXY = self.latLngToXY(14.599512, 120.984219);
+                        break;
+                    case "NOR":
+                        originXY = self.latLngToXY(59.913869, 10.752245);
+                        break;
+                    default: 
+                        originXY = self.path.centroid(svg.select('path.' + datum.origin).data()[0]);
+                }
             } else {
               originXY = self.latLngToXY(val(datum.origin.latitude, datum), val(datum.origin.longitude, datum))
             }
 
             if (typeof datum.destination === 'string') {
-              if (datum.destination == "USA") { destXY = self.latLngToXY(41.140276, -100.760145); }
-              else if (datum.destination == "CAN") { destXY = self.latLngToXY(56.624472, -114.665293); }
-              else if (datum.destination == "JPN") { destXY = self.latLngToXY(35.689487, 139.691706); }
-              else if (datum.destination == "CHL") { destXY = self.latLngToXY(-33.448890, -70.669265); }
-              else if (datum.destination == "IDN") { destXY = self.latLngToXY(-6.208763, 106.845599); }
-              else if (datum.destination == "MYS") { destXY = self.latLngToXY(14.599512, 120.984219); }
-              else { destXY = self.path.centroid(svg.select('path.' + datum.destination).data()[0]); }
+              switch (datum.destination) {
+                    case "USA":
+                        destXY = self.latLngToXY(41.140276, -100.760145);
+                        break;
+                    case "CAN":
+                        destXY = self.latLngToXY(56.624472, -114.665293);
+                        break;
+                    case "JPN":
+                        destXY = self.latLngToXY(35.689487, 139.691706);
+                        break;
+                    case "CHL":
+                        destXY = self.latLngToXY(-33.448890, -70.669265);
+                        break;
+                    case "IDN":
+                        destXY = self.latLngToXY(-6.208763, 106.845599);
+                        break;
+                    case "MYS":
+                        destXY = self.latLngToXY(14.599512, 120.984219);
+                        break;
+                    case "NOR":
+                        destXY = self.latLngToXY(59.913869, 10.752245);
+                        break;
+                    default:
+                        destXY = self.path.centroid(svg.select('path.' + datum.destination).data()[0]);
+              }
             } else {
               destXY = self.latLngToXY(val(datum.destination.latitude, datum), val(datum.destination.longitude, datum));
             }
